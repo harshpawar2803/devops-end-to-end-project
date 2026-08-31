ec920c700455c068aea132dad748d66c93313172
harshpawar2803 <harshp.mca_ioe@bkc.met.edu>
2026-08-31 10:54:36 +0530

    add version to app

--- app/app.py
+++ app/app.py
@@ -5,8 +5,8 @@ class Handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "text/plain")
         self.end_headers()
-        self.wfile.write(b"Hello DevOps - Version 4")
+        self.wfile.write(b"Hello DevOps - Version 5")
 
 server = HTTPServer(("0.0.0.0", 8080), Handler)
-print("Server running on port 8080 - Version 4")
+print("Server running on port 8080 - Version 5")
 server.serve_forever()
